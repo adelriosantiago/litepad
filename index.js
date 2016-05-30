35e6cbfd30fa598c9d17f3031717a74f8633f1bf
Alejandro del Rio <adelriosantiago@gmail.com>
2016-05-29 22:44:54 -0700

    Update name

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    fs = require('fs');
+io = require('socket.io').listen(app),
+fs = require('fs');
 
 var runAtPort = 4000;
-	
+
 app.listen(runAtPort);
 console.log("Now listening at port " + runAtPort);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+	fs.readFile(__dirname + '/index.html',
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
-
-
 var socks = [];
-var body = "Welcome to MiniNo Editor";
+var body = "Welcome to Minipad RTC Editor";
 
 io.sockets.on('connection', function (socket) {
-  socks.push(socket);
-  socket.emit('refresh', {body: body});
-  
-  socket.on('refresh', function (body_) {
-    console.log('new body');
-    body = body_;
-  });
-  
-  socket.on('change', function (op) {
-    console.log(op);
-    if (op.origin == '+input' || op.origin == 'paste' || op.origin == '+delete') {
-      socks.forEach(function (sock) {
-        if (sock != socket)
-          sock.emit('change', op);
-      });
-    };
-  });
+	socks.push(socket);
+	socket.emit('refresh', {body: body});
+	
+	socket.on('refresh', function (body_) {
+		console.log('new body');
+		body = body_;
+	});
+	
+	socket.on('change', function (op) {
+		console.log(op);
+		if (op.origin == '+input' || op.origin == 'paste' || op.origin == '+delete') {
+			socks.forEach(function (sock) {
+				if (sock != socket)
+				sock.emit('change', op);
+			});
+		};
+	});
 });
