25f727ce2f95d2529f5fa1d5ea7d1ecd65ef4f4d
Alejandro Santiago <adelriosantiago@gmail.com>
2026-03-11 14:01:50 -0400

    Update application configuration in ecosystem file for production setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,14 @@ const pkg = require("./package.json")
 module.exports = {
   apps: [
     {
-      name: pkg.name,
+      name: `prod@${pkg.name}`,
       script: pkg.main,
       args: "",
+      cwd: __dirname,
+      interpreter: "node",
+      exec_mode: "fork",
       watch: false,
+      instances: 1,
     },
   ],
 }
